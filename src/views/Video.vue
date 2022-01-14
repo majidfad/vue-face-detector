8bf5e4ed7568657ed6145e952666452749125ab2
majid fad <majidfadavi1375@gmail.com>
2022-01-14 19:38:17 +0330

    Feature: Project Initialized!

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,159 +1,291 @@
 <template>
-  <section class="display">
-    <video
-      ref="videoEl"
-      autoplay="true"
-      playsinline
-      @loadedmetadata="runModel"
-    />
-    <canvas ref="canvasEl" />
-    <li class="board">
-      <ul
-        v-for="(item,key) in board"
-        :key="key"
+  <div
+    id="app"
+    class="web-camera-container"
+  >
+    <div class="camera-button">
+      <button
+        type="button"
+        class="button is-rounded"
+        :class="{ 'is-primary' : !isCameraOpen, 'is-danger' : isCameraOpen}"
+        @click="toggleCamera"
       >
-        {{ key }} ： {{ item }}
+        <span v-if="!isCameraOpen">Open Camera</span>
+        <span v-else>Close Camera</span>
+      </button>
+    </div>
+
+    <div
+      v-show="isCameraOpen && isLoading"
+      class="camera-loading"
+    >
+      <ul class="loader-circle">
+        <li />
+        <li />
+        <li />
       </ul>
-    </li>
-  </section>
+    </div>
+
+    <div
+      v-show="isCameraOpen && !isLoading"
+      class="camera-box"
+      :class="{ 'flash' : isShotPhoto }"
+    >
+      <div
+        class="camera-shutter"
+        :class="{'flash' : isShotPhoto}"
+      />
+
+      <video
+        v-show="!isPhotoTaken"
+        ref="camera"
+        :width="450"
+        :height="337.5"
+        autoplay
+      />
+
+      <canvas
+        v-show="isPhotoTaken"
+        id="photoTaken"
+        ref="canvas"
+        :width="450"
+        :height="337.5"
+      />
+    </div>
+
+    <div
+      v-if="isFaceRecognized &&
+        isCameraOpen &&
+        !isLoading"
+      class="camera-shoot"
+    >
+      <button
+        type="button"
+        class="button"
+        @click="takePhoto"
+      >
+        <img src="https://img.icons8.com/material-outlined/50/000000/camera--v2.png">
+      </button>
+    </div>
+
+    <div
+      v-if="isPhotoTaken && isCameraOpen"
+      class="camera-download"
+    >
+      <a
+        id="downloadPhoto"
+        download="my-photo.jpg"
+        class="button"
+        role="button"
+        @click="downloadImage"
+      >
+        Download
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 import * as faceAPI from 'face-api.js'
-import { onMounted, reactive, ref } from 'vue'
 export default {
-  name: 'Video',
-  setup () {
-    const initParams = reactive({
-      modelUri: '/models',
-      option: new faceAPI.SsdMobilenetv1Options({ minConfidence: 0.5 })
-    })
-    const constraints = reactive({
-      video: {
-        width: {
-          min: 320,
-          ideal: 1280,
-          max: 1920
-        },
-        height: {
-          min: 240,
-          ideal: 720,
-          max: 1080
-        },
-        frameRate: {
-          min: 15,
-          ideal: 30,
-          max: 60
-        },
-        facingMode: 'environment'
-      }
-    })
-    const videoEl = ref(null)
-    const canvasEl = ref(null)
-    const board = reactive({
-      realTimeCounts: 0,
-      male: 0,
-      female: 0,
-      fps: 0
-    })
-
-    let forwardTimes = []
-
-    /**
-     * caculate fps for detection
-     * @function
-     * @param number
-     */
-    const updateTimeStats = (timeInMs) => {
-      forwardTimes = [timeInMs].concat(forwardTimes).slice(0, 30)
-      const avgTimeInMs = forwardTimes.reduce((total, t) => total + t) / forwardTimes.length
-      board.fps = faceAPI.utils.round(1000 / avgTimeInMs)
+  data () {
+    return {
+      isCameraOpen: false,
+      isPhotoTaken: false,
+      isShotPhoto: false,
+      isLoading: false,
+      link: '#',
+      interval: null,
+      isFaceRecognized: false
     }
+  },
+  async beforeMount () {
+    await faceAPI.nets.ssdMobilenetv1.loadFromUri('/models')
+    await faceAPI.nets.ageGenderNet.loadFromUri('/models')
+  },
 
-    /**
-       * @function
-       * @description detect input video
-       */
-    const runModel = async () => {
-      const beforeDetect = Date.now()
-      const result = await faceAPI.detectAllFaces(videoEl.value, initParams.option).withAgeAndGender()
-      updateTimeStats(Date.now() - beforeDetect)
-
+  methods: {
+    async runModel () {
+      const result = await faceAPI.detectAllFaces(this.$refs.camera, await new faceAPI.SsdMobilenetv1Options({ minConfidence: 0.1 })).withAgeAndGender()
       if (result) {
-        const dims = faceAPI.matchDimensions(canvasEl.value, videoEl.value, true)
+        const dims = faceAPI.matchDimensions(this.$refs.canvas, this.$refs.canvas, true)
         const resizeResults = faceAPI.resizeResults(result, dims)
-        board.realTimeCounts = resizeResults.length
-        board.male = resizeResults.filter(data => data.gender === 'male').length
-        board.female = resizeResults.filter(data => data.gender === 'female').length
-        faceAPI.draw.drawDetections(canvasEl.value, resizeResults)
-      }
-      setTimeout(() => runModel())
-    }
-
-    onMounted(() => {
-      /**
-       * @function
-       * @description load the trained model
-       */
-      const initModel = async () => {
-        await faceAPI.nets.ssdMobilenetv1.loadFromUri(initParams.modelUri)
-        await faceAPI.nets.ageGenderNet.loadFromUri(initParams.modelUri)
-      }
-
-      /**
-       * startup webcam
-       * @function
-       */
-      const startStream = async () => {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia(constraints)
-          videoEl.value.srcObject = stream
-        } catch (error) {
-          console.error(error.message)
+        // await faceAPI.draw.drawDetections(this.$refs.canvas, resizeResults)
+        if (resizeResults.length > 0) {
+          this.isFaceRecognized = true
+          clearInterval(this.interval)
         }
       }
+    },
+    toggleCamera () {
+      if (this.isCameraOpen) {
+        this.isCameraOpen = false
+        this.isPhotoTaken = false
+        this.isShotPhoto = false
+        this.stopCameraStream()
+      } else {
+        this.isCameraOpen = true
+        this.createCameraElement()
+        this.interval = setInterval(() => this.runModel())
+      }
+    },
 
-      initModel()
-      startStream()
-    })
+    createCameraElement () {
+      this.isLoading = true
 
-    return {
-      videoEl,
-      canvasEl,
-      runModel,
-      board
+      const constraints = (window.constraints = {
+        audio: false,
+        video: true
+      })
+
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(stream => {
+          this.isLoading = false
+          this.$refs.camera.srcObject = stream
+        })
+        .catch(error => {
+          this.isLoading = false
+          console.log(error)
+        })
+    },
+
+    stopCameraStream () {
+      const tracks = this.$refs.camera.srcObject.getTracks()
+
+      tracks.forEach(track => {
+        track.stop()
+      })
+    },
+
+    takePhoto () {
+      if (!this.isPhotoTaken) {
+        this.isShotPhoto = true
+
+        const FLASH_TIMEOUT = 50
+
+        setTimeout(() => {
+          this.isShotPhoto = false
+        }, FLASH_TIMEOUT)
+      }
+
+      this.isPhotoTaken = !this.isPhotoTaken
+
+      const context = this.$refs.canvas.getContext('2d')
+      context.drawImage(this.$refs.camera, 0, 0, 450, 337.5)
+    },
+
+    downloadImage () {
+      const download = document.getElementById('downloadPhoto')
+      const canvas = document.getElementById('photoTaken').toDataURL('image/jpeg')
+        .replace('image/jpeg', 'image/octet-stream')
+      download.setAttribute('href', canvas)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.display{
-  width: 100%;
-  height: 100vh;
-  position: relative;
-  >video {
-    position: absolute;
-    object-fit: cover;
-    width: 100%;
-    height: 100vh;
-  }
-  >canvas {
-    position: absolute;
-    z-index: 10;
-    width: 100%;
-    height: 100%;
-  }
-  >.board{
-    font-size: 30px;
-    list-style: none;
-    background-color: rgba(255, 255, 255, 0.65);
-    border-radius: 10px;
-    left: 10px;
-    padding: 15px;
-    position: absolute;
-    top: 10px;
-    z-index: 20;
-  }
+body {
+     display: flex;
+     justify-content: center;
+}
+ .web-camera-container {
+     margin-top: 2rem;
+     margin-bottom: 2rem;
+     padding: 2rem;
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     border: 1px solid #ccc;
+     border-radius: 4px;
+     width: 500px;
+}
+ .web-camera-container .camera-button {
+     margin-bottom: 2rem;
+}
+ .web-camera-container .camera-box .camera-shutter {
+     opacity: 0;
+     width: 450px;
+     height: 337.5px;
+     background-color: #fff;
+     position: absolute;
+}
+ .web-camera-container .camera-box .camera-shutter.flash {
+     opacity: 1;
+}
+ .web-camera-container .camera-shoot {
+     margin: 1rem 0;
+}
+ .web-camera-container .camera-shoot button {
+     height: 60px;
+     width: 60px;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     border-radius: 100%;
+}
+ .web-camera-container .camera-shoot button img {
+     height: 35px;
+     object-fit: cover;
+}
+ .web-camera-container .camera-loading {
+     overflow: hidden;
+     height: 100%;
+     position: absolute;
+     width: 100%;
+     min-height: 150px;
+     margin: 3rem 0 0 -1.2rem;
+}
+ .web-camera-container .camera-loading ul {
+     height: 100%;
+     position: absolute;
+     width: 100%;
+     z-index: 999999;
+     margin: 0;
+}
+ .web-camera-container .camera-loading .loader-circle {
+     display: block;
+     height: 14px;
+     margin: 0 auto;
+     top: 50%;
+     left: 100%;
+     transform: translateY(-50%);
+     transform: translateX(-50%);
+     position: absolute;
+     width: 100%;
+     padding: 0;
+}
+ .web-camera-container .camera-loading .loader-circle li {
+     display: block;
+     float: left;
+     width: 10px;
+     height: 10px;
+     line-height: 10px;
+     padding: 0;
+     position: relative;
+     margin: 0 0 0 4px;
+     background: #999;
+     animation: preload 1s infinite;
+     top: -50%;
+     border-radius: 100%;
+}
+ .web-camera-container .camera-loading .loader-circle li:nth-child(2) {
+     animation-delay: 0.2s;
+}
+ .web-camera-container .camera-loading .loader-circle li:nth-child(3) {
+     animation-delay: 0.4s;
+}
+ @keyframes preload {
+     0% {
+         opacity: 1;
+    }
+     50% {
+         opacity: 0.4;
+    }
+     100% {
+         opacity: 1;
+    }
 }
 </style>
